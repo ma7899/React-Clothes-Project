4ab0023688b388a98778be2bbee3d9f0c318309f
ma7899 <skymage78@gmail.com>
2020-03-21 17:41:46 +0430

    created app ands components
    created components and home pages with a little sass.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './Pages/homepage/homepage.component';
 class App extends Component {
 
 
   render(){
     return(
       <div className ='App'>
-      Hello world
+      <HomePage/>
       </div>
     )
   };
