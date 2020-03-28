4ab3e1d4c7856606977e44b57e8d3687acee5460
ma7899 <skymage78@gmail.com>
2020-03-28 04:36:07 +0430

    routing and shop page
    added routing and shop content of shop page

---
+++ src/Components/collection-item/collection-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './collection-item.styels.scss';
+
+const CollectionItem = ({ id, name, price, imageUrl}) => (
+    <div className='collection-item'>
+        <div className = 'image'>
+
+        </div>
+    </div>
+)
